6735c0deda26493ed2fafe42c09724390819be7b
Pietro Strimber <strimberpietro@gmail.com>
2026-08-22 18:07:49 -0300

    fix: fixed Footer and IconTec

--- src/components/IconTec.tsx
+++ src/components/IconTec.tsx
@@ -4,7 +4,7 @@ type IconTecProps = {
 
 const IconTec = ({ title }: IconTecProps) => {
   return (
-    <div className="flex flex-row items-center rounded-full bg-surface border-accent border-1 p-2 px-4 text-xs font-sans hover:border-primary transition-discrete duration-300">
+    <div className="flex flex-row items-center rounded-full bg-surface border-accent border p-2 px-4 text-xs font-sans hover:border-primary transition-discrete duration-300">
       <h1>{title}</h1>
     </div>
   );
