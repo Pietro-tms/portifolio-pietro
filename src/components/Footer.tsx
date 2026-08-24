4fd66f81cbc003aac0db382ddb15c2915fd98730
Pietro Strimber <strimberpietro@gmail.com>
2026-08-24 17:55:21 -0300

    fix: fixed the footer on mobile

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <footer className="border-t-accent border-t flex flex-col w-full min-h-20 md:max-w-screen justify-center items-center bg-background">
+    <footer className="border-t-accent border-t flex flex-col min-w-full min-h-20 md:max-w-screen justify-center items-center bg-background md:p-0 p-4">
       <div className="bg-background w-full h-full md:max-w-[75%] flex flex-row justify-between items-center">
         <h1 className="text-gray-400 font-normal">
           Designed & Developed by Pietro Strimber
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,17 +4,19 @@ import About from "../components/About";
 import Technologies from "../components/Technologies";
 import Projects from "../components/Projects";
 import Footer from "../components/Footer";
-import Contact  from "../components/Contact";
+import Contact from "../components/Contact";
 
 const Home = () => {
   return (
-    <div className="bg-background text-primary flex flex-col items-center h-full w-full md:p-0 p-4 py-0">
+    <div className="bg-background text-primary flex flex-col items-center h-full w-full md:p-0 py-0">
       <NavBar />
-      <Pietro />
-      <About />
-      <Technologies />
-      <Projects />
-      <Contact/>
+      <main className="bg-background text-primary flex flex-col items-center h-full w-full md:p-0 p-4 py-0">
+        <Pietro />
+        <About />
+        <Technologies />
+        <Projects />
+        <Contact />
+      </main>
       <Footer />
     </div>
   );
